b866320c04e11ab5c40e04e5640b01b3fa0f1f15
Cristiano <weydsoncristiano@hotmail.com>
2022-07-04 10:58:28 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let botaoCorrida = document.getElementById("start-race-btn");
 let carro1 = document.querySelector(".car1");
 let carro2 = document.querySelector(".car2");
 let carro3 = document.querySelector(".car3");
+let carro4 = document.querySelector(".car4");
 
 function resetCars() {
 carro1.style.marginLeft = "1px";
 carro2.style.marginLeft = "1px";
 carro3.style.marginLeft = "1px";
+carro4.style.marginLeft = "1px";
 
 }
 
@@ -18,6 +20,9 @@ carro2.style.marginLeft =
 parseInt(carro2.style.marginLeft) + Math.random() * 200 + "px";
 carro3.style.marginLeft =
 parseInt(carro3.style.marginLeft) + Math.random() * 200 + "px";
+carro4.style.marginLeft =
+parseInt(carro4.style.marginLeft) + Math.random() * 200 + "px";
+
 
 if (parseInt(carro1.style.marginLeft) > window.innerWidth) {
 alert("Ferrari-599x venceu!!!");
@@ -31,6 +36,10 @@ if (parseInt(carro3.style.marginLeft) > window.innerWidth) {
 alert("Gallardo gtr venceu!!!");
 resetCars();
 }
+if (parseInt(carro4.style.marginLeft) > window.innerWidth) {
+    alert("Toyota Supra venceu!!!");
+    resetCars();
+    }
 });
 
 botaoCorrida.addEventListener("mouseover", function (event) {
